dbd30eaacff71aba414ea4025f6a101239fe9c38
Francesco Lerro <flerro@thefork.com>
2023-02-01 19:42:42 +0100

    feat: execute base directory
    - add operator.py
    - move run method from cli.py to operator.py

--- mailoperator/cli.py
+++ mailoperator/cli.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 import logging
 
@@ -9,8 +7,10 @@ import operations as op
 def parse_options():
     parser = argparse.ArgumentParser(description="Perform operations on Gmail messages matching a search expression")
     parser.add_argument('search_expr', type=str, help='message search expression')
+    parser.add_argument('-u', '--account', type=str,
+                        help='use non default authentication file', default='token.pickle')
     parser.add_argument('-n', '--max-messages', type=int,
-                        help='max number messages to return as search results', default=10)
+                        help='max number of messages to process (default: 10)', default=10)
     parser.add_argument('--cp', help='download matching messages', action='store_true')
     parser.add_argument('--rm', help='permanently delete matching messages', action='store_true')
 
@@ -37,17 +37,14 @@ def run():
         search_expr = opts.search_expr
 
         if opts.cp:
-            op.download_messages(search_expr, opts.max_messages)
+            op.download_messages(search_expr, opts.max_messages, opts.account)
         elif opts.rm:
-            op.delete_messages(search_expr, opts.max_messages)
+            op.delete_messages(search_expr, opts.max_messages, opts.account)
         else:
-            op.preview_messages(search_expr, opts.max_messages)
+            op.preview_messages(search_expr, opts.max_messages, opts.account)
     except KeyboardInterrupt:
         log.warning("Bye!")
     except Exception as e:
         log.warning("Sorry, something did not work as expected... ", str(e))
 
-
-if __name__ == "__main__":
-    run()
 
