48876176c580206eb8a56cf410f8dfb6a5b57927
Francesco Lerro <flerro@thefork.com>
2023-02-01 19:50:54 +0100

    feat: multi-account auth, download path derived from last label

--- mailoperator/gmail_api.py
+++ mailoperator/gmail_api.py
@@ -15,22 +15,21 @@ CLIENT_SECRET = 'client_secret.json'
 log = logging.getLogger()
 
 
-def gmail_client():
+def gmail_client(account_name='default'):
     """
     Generate a new GMail API client, handling authorization. 
     
-    The 'token.pickle' file stores the user's access and refresh tokens.
-    Auth credentials are loaded from token.pickle if the file is present
+    The '<account_name>.pickle' file stores the user's access and refresh tokens.
+    Auth credentials are loaded from <account_name>.pickle if the file is present
     Otherwise, if there are no (valid) credentials available, the log in 
     flow is triggered.
 
-    The 'token.pickle' file is created automatically when the authorization 
+    The '<account_name>.pickle' file is created automatically when the authorization
     flow completes for the first time.
     
     """
     creds = None
-    token_pickle = "token.pickle"
-
+    token_pickle = account_name + '.pickle'
     if os.path.exists(token_pickle):
         with open(token_pickle, "rb") as token:
             creds = pickle.load(token)
@@ -46,7 +45,7 @@ def gmail_client():
     return build('gmail', 'v1', credentials=creds)
 
 
-def preview_metadata(gmail, message):
+def preview_metadata(gmail, message, options):
     """
     Log in output GMail message metadata, including: 
     subject, sender, received date, size, labels.
@@ -54,18 +53,20 @@ def preview_metadata(gmail, message):
 
     Args:
         gmail: a GMail API client
-    """    
+        :param options: needed for method signature compatibility, but not used
+    """
     msg = gmail.users().messages().get(userId='me', id=message['id'], format='metadata').execute()    
     headers = utils.extract_message_metadata(msg)
-    log.info("Match... %s, Labels: %s", message_info(headers), msg['labelIds'])
+    log.info("Match... %s, Labels: %s", utils.message_info(headers), msg['labelIds'])
 
 
-def delete_message(gmail, message):
+def delete_message(gmail, message, options=None):
     """Permanetly deletes a GMail message
 
     Args:
         gmail: a GMail API client
         message: a GMail message
+        :param options: needed for method signature compatibility, but not used
     """
     msg = gmail.users().messages().get(userId='me', id=message['id'], format='metadata').execute()
     headers = utils.extract_message_metadata(msg)
@@ -73,7 +74,7 @@ def delete_message(gmail, message):
     gmail.users().messages().delete(userId='me', id=message['id']).execute()
 
 
-def search_messages(gmail, search_expr, action=preview_metadata, max_messages=10):
+def search_messages(gmail, search_expr, action=preview_metadata, max_messages=10, options={}):
     """
     Search for GMail messages mathing the given search expression, then perform 
     an operation on eacn message. Default operation is `preview_metadata`.
@@ -83,14 +84,16 @@ def search_messages(gmail, search_expr, action=preview_metadata, max_messages=10
         search_expr: search expression
         action: operation to execute on each matching message
         max_messages: max messages to process 
+        :param options: additional parameters for downstream operations
     """
     msg_count = 0
-    result = gmail.users().messages().list(userId='me',q=search_expr).execute()
+    result = gmail.users().messages().list(userId='me', q=search_expr).execute()
 
     if 'messages' in result:
         for message in result['messages']:
-            if msg_count >= max_messages: return
-            action(gmail, message)
+            if msg_count >= max_messages:
+                return
+            action(gmail, message, options)
             msg_count += 1
 
     while 'nextPageToken' in result:
@@ -98,29 +101,32 @@ def search_messages(gmail, search_expr, action=preview_metadata, max_messages=10
         result = gmail.users().messages().list(userId='me',q=search_expr, pageToken=page_token).execute()
         if 'messages' in result and msg_count < max_messages:
             for message in result['messages']:
-                if msg_count >= max_messages: return
-                action(gmail, message)
+                if msg_count >= max_messages:
+                    return
+                action(gmail, message, options)
                 msg_count += 1
 
 
-def download_message(gmail, message, base_path='account'):
+def download_message(gmail, message, options):
     """Store GMail message as an eml file
     
     Args:
         gmail: a GMail API client
         message: a GMail message
-        base_path (str, optional): base ouput path relative to current directory. Defaults to 'account'.
+        options (str, optional): should include information about account
     """
     msg = gmail.users().messages().get(userId='me', id=message['id'], format='metadata').execute()
     headers = utils.extract_message_metadata(msg)
 
-    log.info("Archiving... %s", utils.message_info(headers))
+    log.info("Dloading... %s", utils.message_info(headers))
 
     msg = gmail.users().messages().get(userId='me', id=message['id'], format='raw').execute()
     raw = msg['raw']
 
+    base_path = options['account'] if 'account' in options else 'account'
     recv_date = headers['date'].strftime('%Y-%m-%d').replace('-', os.path.sep)
-    path = os.path.join(base_path, 'inbox', recv_date)
+    path = os.path.join(base_path, msg['labelIds'][-1], recv_date)
+
     if not os.path.isdir(path):
         os.makedirs(path)
     out_file = os.path.join(path, '%s.eml' % utils.safe_file_name(headers['subject']))
